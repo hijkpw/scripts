d85d3f88ea19de4daba2203f2760ed704f0ba990
hijkpw <58697465+hijkpw@users.noreply.github.com>
2019-12-28 11:52:45 +0800

    Update centos_install_v2ray.sh

--- centos_install_v2ray.sh
+++ centos_install_v2ray.sh
@@ -145,6 +145,8 @@ function showTip()
     echo -e "v2ray配置文件：${red}/etc/v2ray/config.json${plain}，请按照自己需要进行修改"         
     echo  
     echo  如果连接不成功，请注意查看安全组/防火墙是否已放行端口
+    echo 
+    echo  如有其他问题，请到 https://www.hijk.pw 留言反馈
 
     if [ "${bbr}" == "false" ]; then
         echo  
