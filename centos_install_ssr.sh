70de1b561ea12489e13e650a64610ea39f52994e
hijkpw <58697465+hijkpw@users.noreply.github.com>
2020-01-04 21:00:42 +0800

    Update centos_install_ssr.sh

--- centos_install_ssr.sh
+++ centos_install_ssr.sh
@@ -52,15 +52,15 @@ function preinstall()
         ln -s /usr/bin/python3 /usr/bin/python
     fi
     yum install -y nginx
-    wget 'https://github.com/hijkpw/scripts/raw/master/Flatfy%20V3.zip' -O theme.zip
-    unzip theme.zip
-    rm -rf __MACOSX/
-    res=`diff Flatfy\ V3/index.html /usr/share/nginx/html/index.html`
-    if [ "$res" != "" ]; then
+    res=`cat /usr/share/nginx/html/index.html| grep Flatfy`
+    if [ "${res}" = "" ]; then
+        wget 'https://github.com/hijkpw/scripts/raw/master/Flatfy%20V3.zip' -O theme.zip
+        unzip theme.zip
+        rm -rf __MACOSX/
         mv /usr/share/nginx/html/index.html /usr/share/nginx/html/index.html.bak
         mv Flatfy\ V3/* /usr/share/nginx/html/
+        rm -rf theme.zip Flatfy\ V3
     fi
-    rm -rf theme.zip Flatfy\ V3
     systemctl enable nginx && systemctl start nginx
 
     if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
