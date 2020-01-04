1f33d75ba59198bae0f3acff1ec85e7b5574bc0d
hijkpw <58697465+hijkpw@users.noreply.github.com>
2020-01-04 20:59:59 +0800

    Update centos8_install_ss.sh

--- centos8_install_ss.sh
+++ centos8_install_ss.sh
@@ -47,15 +47,15 @@ function preinstall()
     echo "安装必要软件"
     yum install -y epel-release telnet wget vim net-tools unzip
     yum install -y nginx openssl openssl-devel gettext gcc autoconf libtool automake make asciidoc xmlto udns-devel libev-devel pcre pcre-devel mbedtls mbedtls-devel libsodium libsodium-devel c-ares c-ares-devel
-    wget 'https://github.com/hijkpw/scripts/raw/master/Flatfy%20V3.zip' -O theme.zip
-    unzip theme.zip
-    rm -rf __MACOSX/
-    res=`diff Flatfy\ V3/index.html /usr/share/nginx/html/index.html`
-    if [ "$res" != "" ]; then
+    res=`cat /usr/share/nginx/html/index.html| grep Flatfy`
+    if [ "${res}" = "" ]; then
+        wget 'https://github.com/hijkpw/scripts/raw/master/Flatfy%20V3.zip' -O theme.zip
+        unzip theme.zip
+        rm -rf __MACOSX/
         mv /usr/share/nginx/html/index.html /usr/share/nginx/html/index.html.bak
         mv Flatfy\ V3/* /usr/share/nginx/html/
+        rm -rf theme.zip Flatfy\ V3
     fi
-    rm -rf theme.zip Flatfy\ V3
     systemctl enable nginx && systemctl start nginx
 
     if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
