9870c4812bf9230d8e137622e69dcd2f729fccc0
hijk <58697465+hijkpw@users.noreply.github.com>
2020-02-09 21:08:11 +0800

    Update centos7_install_ss.sh

--- centos7_install_ss.sh
+++ centos7_install_ss.sh
@@ -173,6 +173,7 @@ function installSS()
 EOF
     systemctl enable shadowsocks-libev
     systemctl restart shadowsocks-libev
+    sleep 3
     res=`netstat -nltp | grep ${port} | grep 'ss-server'`
     if [ "${res}" = "" ]; then
         echo "ss启动失败，请检查端口是否被占用！"
