cdbcaf29ea507ea0be7eaaa547652af8680b568e
hijk <58697465+hijkpw@users.noreply.github.com>
2020-05-19 19:29:45 +0800

    Update centos_install_v2ray.sh

--- centos_install_v2ray.sh
+++ centos_install_v2ray.sh
@@ -75,6 +75,10 @@ function preinstall()
     fi
     echo "安装必要软件"
     yum install -y epel-release telnet wget vim net-tools ntpdate unzip
+    res=`which wget`
+    [ "$?" != "0" ] && yum install -y wget
+    res=`which netstat`
+    [ "$?" != "0" ] && yum install -y net-tools
     yum install -y nginx
     systemctl enable nginx && systemctl start nginx
 
