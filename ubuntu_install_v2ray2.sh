8157d2647c180d7e78cf631138b97bd25520d159
hijk <hijk.pw@protonmail.ch>
2020-11-07 17:10:04 +0800

    fix(ubuntu_install_v2ray2.sh): fix install python3 bug

--- ubuntu_install_v2ray2.sh
+++ ubuntu_install_v2ray2.sh
@@ -261,7 +261,7 @@ getCert() {
 
         res=`which pip3`
         if [[ "$?" != "0" ]]; then
-            yum install -y python3 python3-pip
+            apt install -y python3 python3-pip
         fi
         res=`which pip3`
         if [[ "$?" != "0" ]]; then
