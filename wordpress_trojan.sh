24503552a27eef6daaabd0d73a87e4cf89f1f728
hijk <hijk.pw@protonmail.ch>
2020-12-27 14:04:41 +0800

    improvement(wordpress): add imagick module for wordpress

--- wordpress_trojan.sh
+++ wordpress_trojan.sh
@@ -113,13 +113,13 @@ installPHP() {
             sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/remi.repo
             dnf module install -y php:remi-7.4
         fi
-        $CMD_INSTALL php-cli php-fpm php-bcmath php-gd php-mbstring php-mysqlnd php-pdo php-opcache php-xml php-pecl-zip
+        $CMD_INSTALL php-cli php-fpm php-bcmath php-gd php-mbstring php-mysqlnd php-pdo php-opcache php-xml php-pecl-zip  php-pecl-imagick
     else
         $CMD_INSTALL lsb-release gnupg2
         wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add -
         echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php7.list
         $PMT update
-        $CMD_INSTALL php7.4-cli php7.4-fpm php7.4-bcmath php7.4-gd php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-xml php7.4-zip php7.4-json
+        $CMD_INSTALL php7.4-cli php7.4-fpm php7.4-bcmath php7.4-gd php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-xml php7.4-zip php7.4-json php7.4-imagick
         update-alternatives --set php /usr/bin/php7.4
     fi
     systemctl enable $PHP_SERVICE
