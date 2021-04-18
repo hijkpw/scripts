98dd4f5828224ed0f72d294890a27fe97682c7c8
hijk <hijk.pw@protonmail.ch>
2021-04-18 21:29:59 +0800

    remove sudo

--- mtproto.sh
+++ mtproto.sh
@@ -131,7 +131,7 @@ installDocker() {
 			curl \
 			gnupg-agent \
 			software-properties-common
-        curl -fsSL https://download.docker.com/linux/$OS/gpg | sudo apt-key add -
+        curl -fsSL https://download.docker.com/linux/$OS/gpg | apt-key add -
         add-apt-repository \
             "deb [arch=amd64] https://download.docker.com/linux/$OS \
             $(lsb_release -cs) \
