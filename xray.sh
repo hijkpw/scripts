5b921df7a0fd356556baf70851e2501fac3011d5
miaoerwu <593970471@qq.com>
2021-12-18 12:18:19 +0800

    Update xray.sh
    Modify the method to get the new version of xray

--- xray.sh
+++ xray.sh
@@ -180,7 +180,7 @@ getVersion() {
     RETVAL=$?
     CUR_VER="$(normalizeVersion "$(echo "$VER" | head -n 1 | cut -d " " -f2)")"
     TAG_URL="${V6_PROXY}https://api.github.com/repos/XTLS/Xray-core/releases/latest"
-    NEW_VER="$(normalizeVersion "$(curl -s "${TAG_URL}" --connect-timeout 10| grep 'tag_name' | cut -d\" -f4)")"
+    NEW_VER="$(normalizeVersion "$(curl -s "${TAG_URL}" --connect-timeout 10 | sed "s/,/\n/g" | grep 'tag_name' -m 1 | cut -d\" -f 4)")"
 
     if [[ $? -ne 0 ]] || [[ $NEW_VER == "" ]]; then
         colorEcho $RED " 检查Xray版本信息失败，请检查网络"
