763bba4e3a13a04f39b36b9d65faf97e0a11a829
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-02-14 11:16:21 +0800

    Update xray1.sh

--- xray1.sh
+++ xray1.sh
@@ -277,7 +277,7 @@ getData() {
 			CERT_FILE="/usr/local/etc/xray/${DOMAIN}.pem"
 			KEY_FILE="/usr/local/etc/xray/${DOMAIN}.key"
 		else
-			resolve=$(curl -sL https://hijk.art/hostip.php?d=${DOMAIN})
+			resolve=$(curl -sL ipget.net/?ip=${DOMAIN})
 			res=$(echo -n ${resolve} | grep ${IP})
 			if [[ -z "${res}" ]]; then
 				colorEcho ${BLUE} "${DOMAIN} 解析结果：${resolve}"
