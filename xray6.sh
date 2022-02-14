91ebd7a21ce438172e8b8759e078bb57ecf4d11d
misakano7545 <misakano7545@protonmail.com>
2022-02-14 16:43:28 +0800

    1

--- xray6.sh
+++ xray6.sh
@@ -444,7 +444,7 @@ getCert() {
 		~/.acme.sh/acme.sh --upgrade --auto-upgrade
 		~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 		if [[ "$BT" == "false" ]]; then
-			if [ -n ($V6_PROXY) ]; then
+			if [ -n $V6_PROXY ]; then
 				~/.acme.sh/acme.sh --issue -d $DOMAIN --keylength ec-256 --pre-hook "systemctl stop nginx" --post-hook "systemctl restart nginx" --standalone --listen-v6
 			else
 				~/.acme.sh/acme.sh --issue -d $DOMAIN --keylength ec-256 --pre-hook "systemctl stop nginx" --post-hook "systemctl restart nginx" --standalone
