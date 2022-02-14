f119f1b37d12d5787c385a94de3d653c4aff96ae
misakano7545 <misakano7545@protonmail.com>
2022-02-14 17:05:03 +0800

    1

--- acme3.sh
+++ acme3.sh
@@ -153,19 +153,21 @@ menu() {
 	echo "                           "
 	red "=================================="
 	echo "                           "
-	green "1. 申请证书（自动识别单域名与泛域名）"
+	green "1. 安装Acme.sh并申请证书"
 	green "2. 查询、撤销并删除当前已申请的域名证书"
 	green "3. 手动续期域名证书"
-	green "4. 更新脚本"
+	green "4. 卸载Acme.sh"
+	green "5. 更新脚本"
 	green "0. 退出"
 	echo "         "
 	read -p "请输入数字:" NumberInput
 	case "$NumberInput" in
-	1) acme ;;
-	2) certificate ;;
-	3) acmerenew ;;
-	4) upgrade ;;
-	0) exit 1 ;;
+		1) acme ;;
+		2) certificate ;;
+		3) acmerenew ;;
+		4 ) uninstall ;;
+		5) upgrade ;;
+		0) exit 1 ;;
 	esac
 }
 
