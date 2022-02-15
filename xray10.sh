93057fb5f082ff79e090fc92bb0a8ca4b14ae3c0
misakano7545 <misakano7545@protonmail.com>
2022-02-15 09:07:24 +0800

    1

--- xray10.sh
+++ xray10.sh
@@ -31,8 +31,8 @@ checkwarp(){
 }
 
 V6_PROXY=""
-IP=$(curl -s4m2 https://ip.gs)
-[[ "$?" != "0" ]] && IP=$(curl -s6m2 https://ip.gs) && V6_PROXY="https://gh-proxy-misakano7545.koyeb.app/"
+IP=$(curl -s4m8 https://ip.gs)
+[[ "$?" != "0" ]] && IP=$(curl -s6m8 https://ip.gs) && V6_PROXY="https://gh-proxy-misakano7545.koyeb.app/"
 [ -n $V6_PROXY ] && echo -e nameserver 2a01:4f8:c2c:123f::1 > /etc/resolv.conf
 
 BT="false"
