736f1272daf1849e3e8579105f09f5df79547111
misakano7545 <misakano7545@protonmail.com>
2022-02-17 20:01:49 +0800

    1

---
+++ argoFile.sh
@@ -0,0 +1,19 @@
+read -p "请输入隧道名称：" tunnelName
+read -p "请输入隧道UUID：" tunnelUUID
+read -p "请输入传输协议（默认http）：" tunnelProtocol
+if [ -z $tunnelProtocol ]; then
+    tunnelProtocol="http"
+fi
+read -p "请输入域名：" tunnelDomain
+read -p "请输入反代端口：" tunnelPort
+cat <<EOF > ~/tunnel1.yml
+tunnel: $tunnelName
+credentials-file: /root/.cloudflared/$tunnelUUID.json
+originRequest:
+  connectTimeout: 30s
+  noTLSVerify: true
+ingress:
+  - hostname: $tunnelDomain
+    service: $tunnelProtocol://localhost:$tunnelPort
+  - service: http_status:404
+EOF
