87d9061d826a53a7cf7813283f72fff46bc9f637
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-02-23 15:51:20 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -44,6 +44,7 @@ echo "                           "
 red "=================================="
 echo "                           "
 read -p "设置root密码:" password
+[ -z $password ] && red "未设置密码！" && exit 1
 echo root:$password | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
@@ -51,3 +52,4 @@ sudo service sshd restart
 green "VPS用户名：root"
 green "vps密码：$password"
 echo "请妥善保存好登录信息！然后重启VPS确保设置已保存！"
+exit 1
