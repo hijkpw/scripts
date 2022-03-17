2b3ec420038275fe8970610bfc79922585993a08
misakano7545 <misakano7545@protonmail.com>
2022-03-18 06:41:14 +0800

    UP

--- warp/warp64.sh
+++ warp/warp64.sh
@@ -30,3 +30,39 @@ for ((int = 0; int < ${#REGEX[@]}; int++)); do
 done
 
 [[ -z $SYSTEM ]] && red "不支持当前VPS的系统，请使用主流操作系统" && exit 1
+
+arch=`uname -m`
+main=`uname  -r | awk -F . '{print $1 }'`
+minor=`uname -r | awk -F . '{print $2}'`
+vpsvirt=`systemd-detect-virt`
+
+install_wireguard_centos(){
+    ${PACKAGE_UPDATE[int]}
+    ${PACKAGE_INSTALL[int]} epel-release
+    ${PACKAGE_INSTALL} net-tools wireguard-tools
+    if [ "$main" -lt 5 ]|| [ "$minor" -lt 6 ]; then 
+        if [[ ${vpsvirt} == "kvm" || ${vpsvirt} == "xen" || ${vpsvirt} == "microsoft" ]]; then
+            vsid=`grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1`
+            curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-$vsid/jdoss-wireguard-epel-$vsid.repo
+            yum -y install epel-release wireguard-dkms
+        fi
+    fi
+}
+
+install_wireguard_debian(){
+    ${PACKAGE_UPDATE[int]}
+    ${PACKAGE_INSTALL[int]} lsb-release
+    echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | tee /etc/apt/sources.list.d/backports.list
+    ${PACKAGE_UPDATE}
+    ${PACKAGE_INSTALL} --no-install-recommends net-tools iproute2 openresolv dnsutils wireguard-tools
+    if [ "$main" -lt 5 ]|| [ "$minor" -lt 6 ]; then
+        if [[ ${vpsvirt} == "kvm" || ${vpsvirt} == "xen" || ${vpsvirt} == "microsoft" ]]; then
+            ${PACKAGE_INSTALL} --no-install-recommends linux-headers-$(uname -r);apt -y --no-install-recommends install wireguard-dkms
+        fi
+    fi
+}
+
+install_wireguard_ubuntu(){
+    ${PACKAGE_UPDATE[int]}
+    ${PACKAGE_INSTALL[int]} --no-install-recommends net-tools iproute2 openresolv dnsutils wireguard-tools
+}
