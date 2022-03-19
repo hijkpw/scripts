11b296485ea32d7c073ca9512822cda5fdec812f
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-03-19 10:58:06 +0800

    Update switch.sh

--- warp_cli/switch.sh
+++ warp_cli/switch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
-red() {}
+red() {
     echo -e "\033[31m\033[01m$1\033[0m"
 } 
 green() {
     echo -e "\033[32m\033[01m$1\033[0m"
 } 
-yellow() {.
+yellow() {
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
