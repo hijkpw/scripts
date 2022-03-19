cabd4a9bd4446491324022ef5c356393d0dd862e
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-03-19 10:55:26 +0800

    Update changeport.sh

--- warp_cli/changeport.sh
+++ warp_cli/changeport.sh
@@ -29,7 +29,7 @@ done
 
 [[ -z $SYSTEM ]] && red "不支持当前VPS的系统，请使用主流操作系统" && exit 1
 
-[[ -z $(warp-cli >/dev/null 2>&1) ]] && red "WARP-Cli未安装，脚本即将退出！" && exit 1
+[[ -z $(type -P warp-cli) ]] && red "WARP-Cli代理模式未安装，脚本即将退出！" && exit 1
 
 changeport(){
     if [[ $(warp-cli --accept-tos status) =~ Connected ]]; then
