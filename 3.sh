12d87d31e9681c3c0ebf72ad6127bd327951a421
misakano7545 <misakano7545@protonmail.com>
2022-03-23 12:32:29 +0800

    UP

--- 3.sh
+++ 3.sh
@@ -140,7 +140,7 @@ checktls() {
             yellow "1. 检测防火墙是否打开，如打开请关闭防火墙或放行80端口"
             yellow "2. 检查80端口是否开放或占用"
             yellow "3. 域名触发Acme.sh官方风控，更换域名或等待7天后再尝试执行脚本"
-            yellow "4. 脚本可能跟不上时代，建议截图发布到TG群询问"
+            yellow "4. 脚本可能跟不上时代，建议截图发布到GitHub Issues或TG群询问"
             exit 1
         fi
     fi
