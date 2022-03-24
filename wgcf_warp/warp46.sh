2e07b51875398701a593898e3f8ea27d582994d6
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-03-24 15:38:42 +0800

    Update warp46.sh

--- wgcf_warp/warp46.sh
+++ wgcf_warp/warp46.sh
@@ -162,7 +162,7 @@ get_best_mtu(){
 }
 
 cpto_wireguard(){
-    mkdir /etc/wireguard
+    mkdir /etc/wireguard >/dev/null 2>&1
     mv -f wgcf-profile.conf /etc/wireguard/wgcf.conf
     mv -f wgcf-account.toml /etc/wireguard/wgcf-account.toml
 }
