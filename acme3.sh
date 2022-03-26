ff9257e7646ad91305ab642016d024d36bf4f5b1
misakano7545 <misakano7545@protonmail.com>
2022-03-26 17:35:42 +0800

    UP

--- acme3.sh
+++ acme3.sh
@@ -117,9 +117,12 @@ getDomainCert(){
     ipv4=$(curl -s4m8 https://ip.gs)
     ipv6=$(curl -s6m8 https://ip.gs)
     read -p "请输入需要申请证书的泛域名（输入格式：example.com）：" domain
-    read -p "请复制Cloudflare的Global API Key：" GAK
+    [[ -z $domain ]] && red "未输入域名，无法执行操作！" $$ exit 1
+    read -p "请输入Cloudflare Global API Key：" GAK
+    [[ -z $GAK ]] && red "未输入Cloudflare Global API Key，无法执行操作！" $$ exit 1
     export CF_Key="$GAK"
-    read -p "请输入登录Cloudflare的注册邮箱地址：" CFemail
+    read -p "请输入Cloudflare的登录邮箱：" CFemail
+    [[ -z $domain ]] && red "未输入Cloudflare的登录邮箱，无法执行操作！" $$ exit 1
     export CF_Email="$CFemail"
     if [[ -z $ipv4 ]]; then
         bash ~/.acme.sh/acme.sh --issue --dns dns_cf -d "*.${domain}" -d "${domain}" -k ec-256 --server letsencrypt --listen-v6
