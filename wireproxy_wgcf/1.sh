5a2e736da2de827aaf24b50c11c4e9ca670e14ec
misakano7545 <misakano7545@protonmail.com>
2022-03-27 23:21:11 +0800

    UP

--- wireproxy_wgcf/1.sh
+++ wireproxy_wgcf/1.sh
@@ -8,15 +8,15 @@ done
 chmod +x wgcf-account.toml
 wgcf generate
 chmod +x wgcf-profile.conf
-WgcfPrivateKey=$(cat /etc/wireguard/wgcf.conf | grep PrivateKey | cut -d= -f2)
-WgcfPublicKey=$(cat /etc/wireguard/wgcf.conf | grep PublicKey | cut -d= -f2)
-WgcfEndPointIP=$(cat /etc/wireguard/wgcf.conf | grep Endpoint | cut -d= -f2)
+WgcfPrivateKey=$(cat wgcf-profile.conf | grep PrivateKey | cut -d= -f2)
+WgcfPublicKey=$(cat wgcf-profile.conf | grep PublicKey | cut -d= -f2)
+WgcfEndPointIP=$(cat wgcf-profile.conf | grep Endpoint | cut -d= -f2)
 
 cat <<EOF > ~/WireProxy_WARP.conf
-SelfSecretKey = $WgcfPrivateKey
+SelfSecretKey =$WgcfPrivateKey
 SelfEndpoint = 172.16.0.2
-PeerPublicKey = $WgcfPublicKey
-PeerEndpoint = $WgcfEndPointIP
+PeerPublicKey =$WgcfPublicKey
+PeerEndpoint =$WgcfEndPointIP
 DNS = 1.1.1.1,8.8.8.8,8.8.4.4
 
 [Socks5]
