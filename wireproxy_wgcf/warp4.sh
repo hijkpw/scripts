82469f96f950adbc450f5fb571c3b57eb778fda4
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-03-28 00:22:12 +0800

    Update warp4.sh

--- wireproxy_wgcf/warp4.sh
+++ wireproxy_wgcf/warp4.sh
@@ -31,49 +31,6 @@ for ((int = 0; int < ${#REGEX[@]}; int++)); do
 done
 
 [[ -z $SYSTEM ]] && red "不支持当前VPS的系统，请使用主流操作系统" && exit 1
-[[ -n $(type -P wgcf) ]] && red "Wgcf-WARP已经安装，脚本即将退出" && rm -f warp4d.sh && exit 1
-
-arch=`uname -m`
-main=`uname  -r | awk -F . '{print $1}'`
-minor=`uname -r | awk -F . '{print $2}'`
-vpsvirt=`systemd-detect-virt`
-
-check_tun(){
-    TUN=$(cat /dev/net/tun 2>&1 | tr '[:upper:]' '[:lower:]')
-    [[ ! $TUN =~ 'in bad state' ]] && [[ ! $TUN =~ '处于错误状态' ]] && [[ ! $TUN =~ 'Die Dateizugriffsnummer ist in schlechter Verfassung' ]] && red "检测到未开启TUN模块，请到VPS控制面板处开启" && exit 1
-}#!/bin/bash
-
-red() {
-    echo -e "\033[31m\033[01m$1\033[0m"
-}
-
-green() {
-    echo -e "\033[32m\033[01m$1\033[0m"
-}
-
-yellow() {
-    echo -e "\033[33m\033[01m$1\033[0m"
-}
-
-# 判断系统及定义系统安装依赖方式
-REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
-RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS")
-PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
-PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install")
-PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove")
-PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove")
-
-CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
-
-for i in "${CMD[@]}"; do
-    SYS="$i" && [[ -n $SYS ]] && break
-done
-
-for ((int = 0; int < ${#REGEX[@]}; int++)); do
-    [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
-done
-
-[[ -z $SYSTEM ]] && red "不支持当前VPS的系统，请使用主流操作系统" && exit 1
 # [[ -n $(type -P wgcf) ]] && red "Wgcf-WARP已经安装，脚本即将退出" && rm -f warp4d.sh && exit 1
 
 arch=`uname -m`
@@ -124,13 +81,13 @@ generate_wgcf_config(){
 
 make_wireproxy_file(){
     # read -p "请输入将要设置的Socks5端口：" socks5Port
-    WgcfPrivateKey=$(cat wgcf-profile.conf | grep PrivateKey | cut -d= -f2)
-    WgcfPublicKey=$(cat wgcf-profile.conf | grep PublicKey | cut -d= -f2)
+    WgcfPrivateKey=$(grep PrivateKey wgcf-profile.conf | sed "s/PrivateKey = //g")
+    WgcfPublicKey=$(grep PublicKey wgcf-profile.conf | sed "s/PublicKey = //g")
     cat <<EOF > ~/WireProxy_WARP.conf
 SelfSecretKey = $WgcfPrivateKey
 SelfEndpoint = 172.16.0.2
 PeerPublicKey = $WgcfPublicKey
-PeerEndpoint = 162.159.193.10:2048
+PeerEndpoint = 162.159.193.10:2408
 DNS = 1.1.1.1,8.8.8.8,8.8.4.4
 
 [Socks5]
@@ -162,6 +119,6 @@ check_tun
 install_wgcf
 register_wgcf
 generate_wgcf_config
-make_wireproxy_profile
+make_wireproxy_file
 download_wireproxy
 start_wireproxy_warp
