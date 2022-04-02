f6c970286a99491a1034b9867980a609c4ed28a8
misakano7545 <misakano7545@protonmail.com>
2022-04-02 19:37:46 +0800

    UP

--- screen2.sh
+++ screen2.sh
@@ -31,7 +31,6 @@ for ((int = 0; int < ${#REGEX[@]}; int++)); do
 done
 
 [[ -z $SYSTEM ]] && red "不支持当前VPS系统，请使用主流的操作系统" && exit 1
-
 [[ -z $(type -P screen) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} screen
 
 back2menu() {
