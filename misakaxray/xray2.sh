ab77dd041cde7396915b9dc03d2a6b2e1bd436f9
misakano7545 <misakano7545@protonmail.com>
2022-04-03 10:09:06 +0800

    UP

--- misakaxray/xray2.sh
+++ misakaxray/xray2.sh
@@ -429,7 +429,7 @@ getCert() {
 		curl -sL https://get.acme.sh | sh -s email=hijk.pw@protonmail.sh
 		source ~/.bashrc
 		~/.acme.sh/acme.sh --upgrade --auto-upgrade
-		~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+		~/.acme.sh/acme.sh --set-default-ca --server zerossl
 		if [[ "$BT" == "false" ]]; then
 			if [[ -n $(curl -sm8 ip.sb | grep ":") ]]; then
 				~/.acme.sh/acme.sh --issue -d $DOMAIN --keylength ec-256 --pre-hook "systemctl stop nginx" --post-hook "systemctl restart nginx" --standalone --listen-v6
