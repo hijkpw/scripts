5214c717463f1e0613bd05aad18e1e216e55411a
misakano7545 <misakano7545@protonmail.com>
2022-04-16 19:37:44 +0800

    UP

--- wgs.sh
+++ wgs.sh
@@ -29,3 +29,77 @@ for ((int = 0; int < ${#REGEX[@]}; int++)); do
 done
 
 [[ -z $SYSTEM ]] && red "不支持当前VPS的系统，请使用主流操作系统" && exit 1
+
+check_tun(){
+    TUN=$(cat /dev/net/tun 2>&1 | tr '[:upper:]' '[:lower:]')
+    if [[ ! $TUN =~ 'in bad state' ]] && [[ ! $TUN =~ '处于错误状态' ]] && [[ ! $TUN =~ 'Die Dateizugriffsnummer ist in schlechter Verfassung' ]]; then
+        if [[ $vpsvirt == "openvz" ]]; then
+            wget -N https://raw.githubusercontents.com/Misaka-blog/tun-script/master/tun.sh && bash tun.sh
+        else
+            red "检测到未开启TUN模块，请到VPS控制面板处开启" 
+            exit 1
+        fi
+    fi
+}
+
+checkCentOS8(){
+    if [[ -n $(cat /etc/os-release | grep "CentOS Linux 8") ]]; then
+        yellow "检测到当前VPS系统为CentOS 8，是否升级为CentOS Stream 8以确保软件包正常安装？"
+        read -p "请输入选项 [y/n]：" comfirmCentOSStream
+        if [[ $comfirmCentOSStream == "y" ]]; then
+            yellow "正在为你升级到CentOS Stream 8，大概需要10-30分钟的时间"
+            sleep 1
+            sed -i -e "s|releasever|releasever-stream|g" /etc/yum.repos.d/CentOS-*
+            yum clean all && yum makecache
+            dnf swap centos-linux-repos centos-stream-repos distro-sync -y
+        else
+            red "已取消升级过程，脚本即将退出！"
+            exit 1
+        fi
+    fi
+}
+
+install_wireguard_centos(){
+    ${PACKAGE_INSTALL[int]} epel-release
+    ${PACKAGE_INSTALL[int]} net-tools wireguard-tools iptables
+    if [ "$main" -lt 5 ] || [ "$minor" -lt 6 ]; then 
+        if [[ ${vpsvirt} == "kvm" || ${vpsvirt} == "xen" || ${vpsvirt} == "microsoft" ]]; then
+            vsid=`grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1`
+            curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-$vsid/jdoss-wireguard-epel-$vsid.repo
+            yum -y install epel-release wireguard-dkms
+        fi
+    fi
+}
+
+install_wireguard_debian(){
+    ${PACKAGE_INSTALL[int]} lsb-release
+    echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | tee /etc/apt/sources.list.d/backports.list
+    ${PACKAGE_UPDATE[int]}
+    ${PACKAGE_INSTALL} --no-install-recommends net-tools iproute2 openresolv dnsutils wireguard-tools iptables
+    if [ "$main" -lt 5 ] || [ "$minor" -lt 6 ]; then
+        if [[ ${vpsvirt} == "kvm" || ${vpsvirt} == "xen" || ${vpsvirt} == "microsoft" ]]; then
+            ${PACKAGE_INSTALL} --no-install-recommends linux-headers-$(uname -r);apt -y --no-install-recommends install wireguard-dkms
+        fi
+    fi
+}
+
+install_wireguard_ubuntu(){
+    ${PACKAGE_INSTALL[int]} --no-install-recommends net-tools iproute2 openresolv dnsutils wireguard-tools iptables
+}
+
+install_wireguard(){
+    ${PACKAGE_UPDATE[int]}
+    [[ -z $(type -P curl) ]] && ${PACKAGE_INSTALL[int]} curl
+    [[ -z $(type -P sudo) ]] && ${PACKAGE_INSTALL[int]} sudo
+    [[ $SYSTEM == CentOS ]] && install_wireguard_centos
+    [[ $SYSTEM == Debian ]] && install_wireguard_debian
+    [[ $SYSTEM == Ubuntu ]] && install_wireguard_ubuntu
+    if [[ $vpsvirt =~ lxc|openvz ]]; then
+        wget -N https://cdn.jsdelivr.net/gh/Misaka-blog/Misaka-WARP-Script/wireguard-go -O /usr/bin/wireguard-go
+        chmod +x /usr/bin/wireguard-go
+    fi
+    if [[ $vpsvirt == zvm ]]; then
+        wget -N https://cdn.jsdelivr.net/gh/Misaka-blog/Misaka-WARP-Script/wireguard-go-s390x -O /usr/bin/wireguard-go
+        chmod +x /usr/bin/wireguard-go
+    fi
+}
