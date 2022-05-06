3899ea3d4b2aa5e7174db39420d1f047b8704261
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-05-07 02:40:50 +0800

    Update hysteria.sh

--- hysteria/hysteria.sh
+++ hysteria/hysteria.sh
@@ -20,6 +20,8 @@ PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y inst
 PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove")
 PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove")
 
+[[ $EUID -ne 0 ]] && red "请在root用户下运行脚本" && exit 1
+
 CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')") 
 
 for i in "${CMD[@]}"; do
