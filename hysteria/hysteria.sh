a1e3cbc99e71454d53bb7696bc84c420ba613312
misakano7545 <misakano7545@protonmail.com>
2022-05-13 06:56:05 +0800

    UP

--- hysteria/hysteria.sh
+++ hysteria/hysteria.sh
@@ -203,8 +203,8 @@ installHysteria() {
     checkCentOS8
     install_base
     downloadHysteria
-    read -p "是否安装BBR（y/n，默认n）：" INSTALL_BBR
-    [[ $INSTALL_BBR =~ "y"|"Y" ]] && INSTALL_BBR
+    read -p "是否安装BBR（y/n，默认n）：" INSTALL_BBR_YN
+    [[ $INSTALL_BBR =~ "y"|"Y" ]] && INSTALL_BBR_YN
     makeConfig
     systemctl enable hysteria
     systemctl start hysteria
