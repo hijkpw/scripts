111c22a08ae32690345d0ee296c397a79d619b02
misakano7545 <misakano7545@protonmail.com>
2022-05-13 22:42:23 +0800

    UP

--- misakawarp/misakawarp.sh
+++ misakawarp/misakawarp.sh
@@ -3,12 +3,20 @@
 red() {
     echo -e "\033[31m\033[01m$1\033[0m"
 } 
+
 green() {
     echo -e "\033[32m\033[01m$1\033[0m"
 } 
+
 yellow() {
     echo -e "\033[33m\033[01m$1\033[0m"
 } 
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[36m"
+PLAIN='\033[0m'
 
 # 判断系统及定义系统安装依赖方式
 REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
