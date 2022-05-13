1ef105bd402e34adf4c2b0644b29097dc238ef34
misakano7545 <misakano7545@protonmail.com>
2022-05-13 12:39:03 +0800

    UP

---
+++ misakawarp/misakawarp.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+red() {
+    echo -e "\033[31m\033[01m$1\033[0m"
+} 
+green() {
+    echo -e "\033[32m\033[01m$1\033[0m"
+} 
+yellow() {
+    echo -e "\033[33m\033[01m$1\033[0m"
+} 
+
+# 判断系统及定义系统安装依赖方式
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install")
+PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove")
+PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove")
+
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')") 
+
+for i in "${CMD[@]}"; do
+    SYS="$i" && [[ -n $SYS ]] && break
+done
+
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+done
+
+[[ $EUID -ne 0 ]] && red "注意：请在root用户下运行脚本" && exit 1
+
+archAffix(){
+    case "$(uname -m)" in
+        x86_64|amd64) echo 'amd64' ;;
+        *armv8*|aarch64) echo 'arm64' ;;
+        *s390x*) echo 's390x' ;;
+        *) red " 不支持的CPU架构！" && exit 1 ;;
+    esac
+}
+
+check_status(){
+    IPv4Status=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
+    IPv6Status=$(curl -s6m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
+    # VPSIP变量说明：0为纯IPv6 VPS、1为纯IPv4 VPS、2为原生双栈VPS
+    if [[ -z $IPv4Status ]] && [[ $IPv6Status == "off" ]]; then
+        VPSIP=0
+    fi
+    if [[ $IPv4Status == "off" ]] && [[ -z $IPv6Status ]]; then
+        VPSIP=1
+    fi
+    if [[ $IPv4Status == "off" ]] && [[ $IPv6Status == "off" ]]; then
+        VPSIP=2
+    fi
+}
